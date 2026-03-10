9c0e90eedd5d4b3b97b598927b8ff80ffb3a628a
mattdevops <mattstermh@yahoo.com>
2026-03-10 13:20:31 -0400

    attempt fix

--- src/app/(staff)/staff/api/auth/login/route.ts
+++ src/app/(staff)/staff/api/auth/login/route.ts
@@ -40,6 +40,7 @@ export async function POST(req: Request) {
     sameSite: "lax",
     secure: process.env.NODE_ENV === "production",
     path: "/",
+    maxAge: 60 * 60 * 24 * 30, // 30 days
   });
 
   return res;
